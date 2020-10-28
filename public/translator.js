a747305fef6290866d9029275dbcaacd16a1268c
Menno van Balen <mennovanbalen@gmail.com>
2020-10-28 23:10:38 +0000

    fixed handling for  american and british ONLY, no more one big dictionary..

--- public/translator.js
+++ public/translator.js
@@ -3,74 +3,32 @@ import { americanToBritishSpelling } from "./american-to-british-spelling.js";
 import { americanToBritishTitles } from "./american-to-british-titles.js";
 import { britishOnly } from "./british-only.js";
 
-// const longestDictionary = (ArrayOfDictionaries) => {
-//   let length = 0;
-//   ArrayOfDictionaries.forEach((dictionary) => {
-//     console.log(Object.keys(dictionary).length);
-//     const dictionaryLength = Object.keys(dictionary).length;
-//     if (dictionaryLength > length) length = dictionaryLength;
+// createCompleteDictionary takes in 2 arrays with dictionaries (objects)
+// the first => American to British
+// the second => British to American
+// const createCompleteDictionary = (array_Am_Br, array_Br_Am) => {
+//   const dictionaryAmeBri = {};
+
+//   array_Am_Br.forEach((dictionary) => {
+//     Object.assign(dictionaryAmeBri, dictionary);
 //   });
-//   return length;
+
+//   array_Br_Am.forEach((dictionary) => {
+//     const length = Object.keys(dictionary).length;
+//     for (let i = 0; i < length; i++) {
+//       dictionaryAmeBri[Object.values(dictionary)[i]] = Object.keys(dictionary)[
+//         i
+//       ];
+//     }
+//   });
+//   return dictionaryAmeBri;
 // };
-// //
-// const length = longestDictionary([
-//   americanOnly,
-//   americanToBritishSpelling,
-//   americanToBritishTitles,
-//   britishOnly,
-// ]);
-// let dictionaryAmeBri = {};
-// // dictionaryAmeBri[Object.keys(americanOnly)[0]] = Object.values(americanOnly)[0];
-// for (let i = 0; i <= length; i++) {
-//   if (Object.keys(americanOnly)[i]) {
-//     dictionaryAmeBri[Object.keys(americanOnly)[i]] = Object.values(
-//       americanOnly
-//     )[i];
-//   }
-//   if (Object.keys(americanToBritishSpelling)[i]) {
-//     dictionaryAmeBri[Object.keys(americanToBritishSpelling)[i]] = Object.values(
-//       americanToBritishSpelling
-//     )[i];
-//   }
-//   if (Object.keys(americanToBritishTitles)[i]) {
-//     dictionaryAmeBri[Object.keys(americanToBritishTitles)[i]] = Object.values(
-//       americanToBritishTitles
-//     )[i];
-//   }
-//   if (Object.keys(britishOnly)[i]) {
-//     dictionaryAmeBri[Object.values(britishOnly)[i]] = Object.keys(britishOnly)[
-//       i
-//     ];
-//   }
-// }
-// console.log(dictionaryAmeBri);
 
-// createCompleetDictionary takes in 2 arrays
-// the first with American to Britisch ArrayOfDictionaries
-// the second with Britisch to American
-const createCompleetDictionary = (array_Am_Br, array_Br_Am) => {
-  const dictionaryAmeBri = {};
-
-  array_Am_Br.forEach((dictionary) => {
-    Object.assign(dictionaryAmeBri, dictionary);
-  });
-
-  array_Br_Am.forEach((dictionary) => {
-    const length = Object.keys(dictionary).length;
-    for (let i = 0; i < length; i++) {
-      dictionaryAmeBri[Object.values(dictionary)[i]] = Object.keys(dictionary)[
-        i
-      ];
-    }
-  });
-  return dictionaryAmeBri;
-};
-
-const compleetDictionaryAm_Br = createCompleetDictionary(
-  [americanOnly, americanToBritishSpelling, americanToBritishTitles],
-  [britishOnly]
-);
-console.log(compleetDictionaryAm_Br.crosswalk);
+// const dictionary_Am_Br = createCompleteDictionary(
+//   [americanOnly, americanToBritishSpelling, americanToBritishTitles],
+//   [britishOnly]
+// );
+// console.log(dictionary_Am_Br.crosswalk);
 
 // get the languagePair
 const localeSelect = document.getElementById("locale-select");
@@ -88,9 +46,65 @@ const getText = () => {
   return document.getElementById("text-input").value;
 };
 
+const translatedSentenceDiv = document.getElementById("translated-sentence");
+
 const translate = (languagePair, text) => {
   console.log("languagePair: " + languagePair);
   console.log("text: " + text);
+
+  if (languagePair === "american-to-british") {
+    const dictArray = [
+      americanOnly,
+      americanToBritishSpelling,
+      americanToBritishTitles,
+    ];
+    dictArray.forEach((dictionary) => {
+      for (const [american, british] of Object.entries(dictionary)) {
+        text = text.replace(
+          american,
+          `<span class="highlight">${british}</span>`
+        );
+        text = text.replace(
+          american.charAt(0).toUpperCase() + american.slice(1),
+          `<span class="highlight">${
+            british.charAt(0).toUpperCase() + british.slice(1)
+          }</span>`
+        );
+      }
+    });
+  }
+  if (languagePair === "british-to-american") {
+    const dictArray = [americanToBritishSpelling, americanToBritishTitles];
+    dictArray.forEach((dictionary) => {
+      for (const [american, british] of Object.entries(dictionary)) {
+        text = text.replace(
+          british,
+          `<span class="highlight">${american}</span>`
+        );
+        text = text.replace(
+          british.charAt(0).toUpperCase() + british.slice(1),
+          `<span class="highlight">${
+            american.charAt(0).toUpperCase() + american.slice(1)
+          }</span>`
+        );
+      }
+    });
+
+    for (const [british, american] of Object.entries(britishOnly)) {
+      text = text.replace(
+        british,
+        `<span class="highlight">${american}</span>`
+      );
+      text = text.replace(
+        british.charAt(0).toUpperCase() + british.slice(1),
+        `<span class="highlight">${
+          american.charAt(0).toUpperCase() + american.slice(1)
+        }</span>`
+      );
+    }
+  }
+  console.log(text);
+  translatedSentenceDiv.innerHTML = text;
 };
 
 translateButton.addEventListener("click", () => {
