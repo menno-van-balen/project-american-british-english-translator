ded4339f8161061ff3455d534bd06665122d1267
Menno van Balen <mennovanbalen@gmail.com>
2020-10-28 16:56:18 +0000

    added create one compleet dictionary from multiple dictionaries function

--- public/translator.js
+++ public/translator.js
@@ -1,7 +1,102 @@
-import { americanOnly } from './american-only.js';
-import { britishOnly } from './british-only.js';
-import { americanToBritishSpelling } from './american-to-british-spelling.js';
-import { americanToBritishTitles } from './american-to-british-titles.js';
+import { americanOnly } from "./american-only.js";
+import { americanToBritishSpelling } from "./american-to-british-spelling.js";
+import { americanToBritishTitles } from "./american-to-british-titles.js";
+import { britishOnly } from "./british-only.js";
+
+// const longestDictionary = (ArrayOfDictionaries) => {
+//   let length = 0;
+//   ArrayOfDictionaries.forEach((dictionary) => {
+//     console.log(Object.keys(dictionary).length);
+//     const dictionaryLength = Object.keys(dictionary).length;
+//     if (dictionaryLength > length) length = dictionaryLength;
+//   });
+//   return length;
+// };
+// //
+// const length = longestDictionary([
+//   americanOnly,
+//   americanToBritishSpelling,
+//   americanToBritishTitles,
+//   britishOnly,
+// ]);
+// let dictionaryAmeBri = {};
+// // dictionaryAmeBri[Object.keys(americanOnly)[0]] = Object.values(americanOnly)[0];
+// for (let i = 0; i <= length; i++) {
+//   if (Object.keys(americanOnly)[i]) {
+//     dictionaryAmeBri[Object.keys(americanOnly)[i]] = Object.values(
+//       americanOnly
+//     )[i];
+//   }
+//   if (Object.keys(americanToBritishSpelling)[i]) {
+//     dictionaryAmeBri[Object.keys(americanToBritishSpelling)[i]] = Object.values(
+//       americanToBritishSpelling
+//     )[i];
+//   }
+//   if (Object.keys(americanToBritishTitles)[i]) {
+//     dictionaryAmeBri[Object.keys(americanToBritishTitles)[i]] = Object.values(
+//       americanToBritishTitles
+//     )[i];
+//   }
+//   if (Object.keys(britishOnly)[i]) {
+//     dictionaryAmeBri[Object.values(britishOnly)[i]] = Object.keys(britishOnly)[
+//       i
+//     ];
+//   }
+// }
+// console.log(dictionaryAmeBri);
+
+// createCompleetDictionary takes in 2 arrays
+// the first with American to Britisch ArrayOfDictionaries
+// the second with Britisch to American
+const createCompleetDictionary = (array_Am_Br, array_Br_Am) => {
+  const dictionaryAmeBri = {};
+
+  array_Am_Br.forEach((dictionary) => {
+    Object.assign(dictionaryAmeBri, dictionary);
+  });
+
+  array_Br_Am.forEach((dictionary) => {
+    const length = Object.keys(dictionary).length;
+    for (let i = 0; i < length; i++) {
+      dictionaryAmeBri[Object.values(dictionary)[i]] = Object.keys(dictionary)[
+        i
+      ];
+    }
+  });
+  return dictionaryAmeBri;
+};
+
+const compleetDictionaryAm_Br = createCompleetDictionary(
+  [americanOnly, americanToBritishSpelling, americanToBritishTitles],
+  [britishOnly]
+);
+console.log(compleetDictionaryAm_Br.crosswalk);
+
+// get the languagePair
+const localeSelect = document.getElementById("locale-select");
+
+let languagePair = localeSelect.value;
+
+localeSelect.addEventListener("change", () => {
+  languagePair = localeSelect.value;
+  // console.log("languagePair: " + languagePair);
+});
+
+const translateButton = document.getElementById("translate-btn");
+
+const getText = () => {
+  return document.getElementById("text-input").value;
+};
+
+const translate = (languagePair, text) => {
+  console.log("languagePair: " + languagePair);
+  console.log("text: " + text);
+};
+
+translateButton.addEventListener("click", () => {
+  const text = getText();
+  translate(languagePair, text);
+});
 
 /* 
   Export your functions for testing in Node.
@@ -9,7 +104,5 @@ import { americanToBritishTitles } from './american-to-british-titles.js';
   the client side
 */
 try {
-  module.exports = {
-
-  }
+  module.exports = {};
 } catch (e) {}
